2639c51714fbe73f57dd8b0aa93b905160f9cef5
Aprian Bwire <aprythomas5@gmail.com>
2023-05-14 22:02:19 +0300

    wait fxn

---
+++ wait.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+/**
+ * wait - testing the wait fxn
+ * Return: 0 for success and 1 for failing
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error\n");
+		return (1);
+	}
+	if (child_pid == 0)
+	{
+		printf("Wait for me\n");
+	}
+	else
+	{
+		wait(&status);
+		printf("I Already waited So you are done my boyyy\n");
+	}
+	return (0);
+}
